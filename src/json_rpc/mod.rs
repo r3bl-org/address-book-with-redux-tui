933c39ea084deddbe019b08438e931291ddab528
Nazmul Idris <nazmul@fasterlap.com>
2023-10-03 12:07:01 -0700

    Remove make_api_call_for macro from r3bl_rs_utils_core

--- src/json_rpc/mod.rs
+++ src/json_rpc/mod.rs
@@ -24,3 +24,30 @@ pub mod get_ip_api;
 pub use awair_local_api::*;
 pub use fake_contact_data_api::*;
 pub use get_ip_api::*;
+
+/// Declarative macro to generate the API call functions. This adds the
+/// following:
+/// - `make_request()` async function to call the API.
+/// - `to_string()` function to stringify the struct to JSON.
+/// - impl `Display` trait to for the struct using `to_string()` above.
+#[macro_export]
+macro_rules! make_api_call_for {
+    ($IDENT:ident at $ENDPOINT:ident) => {
+        pub async fn make_request() -> Result<$IDENT, Box<dyn Error>> {
+            let res = reqwest::get($ENDPOINT).await?;
+            let res_text = res.text().await?;
+            let res_json: $IDENT = serde_json::from_str(&res_text)?;
+            Ok(res_json)
+        }
+
+        impl $IDENT {
+            pub fn to_string(&self) -> String { serde_json::to_string(&self).unwrap() }
+        }
+
+        impl Display for $IDENT {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.to_string())
+            }
+        }
+    };
+}
